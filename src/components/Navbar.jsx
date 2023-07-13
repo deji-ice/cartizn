d16aa6e6f81ea9873fd85282b2a3e6dd4bd0d440
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-07-13 21:57:42 +0100

    added svg icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@
 import DensityLargeIcon from '@mui/icons-material/DensityLarge';
 
 const Navbar = () => {
-  return <div className="flex border-[0.5px] border-y-zinc-300 text-white font-thin text-sm h ">
-    <div className="border-[0.5px] flex-8 border-y-zinc-300 p-6">
+  return <div className="flex border-[0.5px] border-[#F5F5F5] text-[#F5F5F5] font-thin text-sm h ">
+    <div className="border-[0.5px] flex-8 border-[#F5F5F5] p-6">
         logo
     </div>
-    <div className=" border-[0.5px] flex-1 border-y-zinc-300 p-6 text-center"> 
+    <div className=" border-[0.5px] flex-1 border-[#F5F5F5] p-6 text-center"> 
         "The ways of <span className="text-[#DEC649] font-light">Carti.</span> are not regular"
     </div>
-    <div className="border-[0.5px] flex-9 border-y-zinc-300 p-6">
-    <DensityLargeIcon />
+    <div className="border-[0.5px] flex-9 border-[#F5F5F5] p-6">
+    <DensityLargeIcon className='text-[#F5F5F5]'/>
     </div>
   </div>;
 };
