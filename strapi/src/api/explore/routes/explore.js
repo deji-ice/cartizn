deb8590aaaf13f205c0c34c84b436cf5dbd12557
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-07-14 21:23:40 -0200

    added strapi

---
+++ strapi/src/api/explore/routes/explore.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * explore router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::explore.explore');
---
+++ strapi/src/api/explore1/controllers/explore1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * explore1 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::explore1.explore1');
