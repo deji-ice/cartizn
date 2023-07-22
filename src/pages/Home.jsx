67b8a76cdc85bbeba8e25d640005241ce623847b
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-07-22 23:02:45 +0100

    conditionals for link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 
 
 const Home = () => {
-  return <div className="lg:h-100% lg:overflow-y-clip xl:h-screen relative overflow-x-clip">
+  return <div className="lg:overflow-y-clip xl:min-h-fit xl:overflow-hidden relative overflow-x-clip">
     <Navbar/>
     <About/>
     <Explore/>
