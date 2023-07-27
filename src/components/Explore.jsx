100140cd804f2451e3777d1c3dfa3ff1f22aab6c
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-07-27 22:26:52 +0100

    custom hook for explore page

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,78 +1,29 @@
-import { useEffect, useState, useRef } from "react";
+import { useRef } from "react";
 import { useMediaQuery } from '@react-hook/media-query';
 import gsap from 'gsap';
 import { Link } from "react-router-dom";
-import { motion } from 'framer-motion';
-import odu from "../assets/Property 1=Default (6).png"
+import useInterval from "../hooks/useInterval";
 
 
 const Explore = () => {
   let imageRef = useRef(null);
   let explore = useRef(null)
 
-  const data1 = [odu, "https://i.imgur.com/Q5CtCJi.png", "https://i.imgur.com/i7A133P.png"];
+  const data1 = ["https://i.imgur.com/Q5CtCJi.png", "https://i.imgur.com/Q5CtCJi.png", "https://i.imgur.com/i7A133P.png"];
   const data2 = ["https://i.imgur.com/a4Abq7x.png", "https://i.imgur.com/fyfzvnX.png", "https://i.imgur.com/rcWZdSy.png"];
   const data3 = ["https://i.imgur.com/JcbsGag.png", "https://i.imgur.com/KMLqgha.png", "https://i.imgur.com/3hRiyi3.png"];
   const data4 = ["https://i.imgur.com/Kc5WiJG.png", "https://i.imgur.com/H0pKlzd.png", "https://i.imgur.com/8nEu8Zu.png"];
   const data5 = ["https://i.imgur.com/wuB6tt1.png", "https://i.imgur.com/NtZ6Hb4.png", "https://i.imgur.com/Qc3QLGk.png"];
-  const [currentIndex1, setCurrentIndex1] = useState(0);
-  const [currentIndex2, setCurrentIndex2] = useState(0);
-  const [currentIndex3, setCurrentIndex3] = useState(0);
-  const [currentIndex4, setCurrentIndex4] = useState(0);
-  const [currentIndex5, setCurrentIndex5] = useState(0);
-  const [nextImage, setNextImage] = useState(null);
   // Use the useMediaQuery hook to get the screen size
   const isXlScreen = useMediaQuery('(min-width: 1280px)');
 
-
-  useEffect(() => {
-    // const fadeIn = () => {
-    //   gsap.from(imageRef.current, {duration:1 ,opacity: 0 ,ease:"easeInOut"})
-    // };
-
-    // const fadeOut = () => {
-    //   gsap.to(imageRef.current, {duration:1 ,opacity: 1 ,ease:"easeInOut", onComplete:changeImage})
-    // };
-
-    // const changeImage = () => {
-    //   fadeIn();
-    //   setNextImage(data1[currentIndex1]);
-    // };
-
-    const interval1 = setInterval(() => {
-      // fadeOut()
-      setCurrentIndex1((prevIndex) => (prevIndex + 1) % data1.length);
-
-    }, 4000); // Adjust the interval for smoother transition
-
-    const interval2 = setInterval(() => {
-      setCurrentIndex2((prevIndex) => (prevIndex + 1) % data2.length);
-    }, 2500);
-
-    const interval3 = setInterval(() => {
-      setCurrentIndex3((prevIndex) => (prevIndex + 1) % data3.length);
-    }, 3000);
-
-    const interval4 = setInterval(() => {
-      setCurrentIndex4((prevIndex) => (prevIndex + 1) % data4.length);
-    }, 3200);
-
-    const interval5 = setInterval(() => {
-      setCurrentIndex5((prevIndex) => (prevIndex + 1) % data5.length);
-    }, 3500);
-
-    return () => {
-      clearInterval(interval1);
-      clearInterval(interval2);
-      clearInterval(interval3);
-      clearInterval(interval4);
-      clearInterval(interval5);
-    };
-  }, [currentIndex1]);
-
-
-
-
+  const {
+    currentIndex1,
+    currentIndex2,
+    currentIndex3,
+    currentIndex4,
+    currentIndex5 
+  } = useInterval(data1, data2, data3, data4, data5)
 
   const handleHover = () => {
     gsap.to(explore.current, { width: "100%", duration: 1, ease: 'power2.inOut' });
@@ -81,6 +32,7 @@ const Explore = () => {
   const handleHoverOut = () => {
     gsap.to(explore.current, { width: "0%", duration: 1, ease: 'power2.inOut' });
   };
+  
   return <section className="flex  xl:h-[30%]  gap-0 p-0">
     {/* TODO */}
     <div className="flex flex-col lg:flex-row xl:flex-1  w-screen  ease-linear duration-500 xl:overflow-x-clip  ">
