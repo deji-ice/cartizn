82e30c9de865591a70fdb89070bd12ae4f503b6a
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-07-30 23:47:41 +0100

    refs

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -8,10 +8,17 @@ import useInterval from "../hooks/useInterval";
 const Explore = () => {
   let explore = useRef(null)
 
+  // Create refs for the images
+  const image1Ref = useRef(null);
+  const image2Ref = useRef(null);
+  const image3Ref = useRef(null);
+  const image4Ref = useRef(null);
+  const image5Ref = useRef(null);
+
   const data1 = ["https://res.cloudinary.com/dhvwthnzq/image/upload/v1690493903/cartiznHD/explore/Property_1_Default_xvgshy.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690493890/cartiznHD/explore/Property_1_Variant2_bvgual.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690493890/cartiznHD/explore/Property_1_Variant3_gyylcb.png"];
   const data2 = ["https://res.cloudinary.com/dhvwthnzq/image/upload/v1690495070/cartiznHD/explore/Property_1_Default_vby3sw.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690495073/cartiznHD/explore/Property_1_Variant2_hnqzgj.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690495070/cartiznHD/explore/Property_1_Variant3_p0pfgv.png"];
   const data3 = ["https://res.cloudinary.com/dhvwthnzq/image/upload/v1690494793/cartiznHD/explore/Property_1_Default_mdlntu.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690494794/cartiznHD/explore/Property_1_Variant2_th0tpo.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690494794/cartiznHD/explore/Property_1_Variant3_ptzymr.png"];
-  const data4 = ["https://res.cloudinary.com/dhvwthnzq/image/upload/v1690494391/cartiznHD/explore/Property_1_Default_dr9nix.png","https://res.cloudinary.com/dhvwthnzq/image/upload/v1690494390/cartiznHD/explore/Property_1_Variant2_rpasup.png","https://res.cloudinary.com/dhvwthnzq/image/upload/v1690494393/cartiznHD/explore/Property_1_Variant3_cj549f.png"];
+  const data4 = ["https://res.cloudinary.com/dhvwthnzq/image/upload/v1690494391/cartiznHD/explore/Property_1_Default_dr9nix.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690494390/cartiznHD/explore/Property_1_Variant2_rpasup.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690494393/cartiznHD/explore/Property_1_Variant3_cj549f.png"];
   const data5 = ["https://res.cloudinary.com/dhvwthnzq/image/upload/v1690495047/cartiznHD/explore/Property_1_Default_teoxbo.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690495047/cartiznHD/explore/Property_1_Variant2_cvbuf8.png", "https://res.cloudinary.com/dhvwthnzq/image/upload/v1690495048/cartiznHD/explore/Property_1_Variant3_ajlld7.png"];
   // Use the useMediaQuery hook to get the screen size
   const isXlScreen = useMediaQuery('(min-width: 1280px)');
@@ -21,8 +28,18 @@ const Explore = () => {
     currentIndex2,
     currentIndex3,
     currentIndex4,
-    currentIndex5 
-  } = useInterval(data1, data2, data3, data4, data5)
+    currentIndex5
+  } = useInterval(
+    data1, 
+    data2, 
+    data3, 
+    data4,
+    data5, 
+    image1Ref,
+    image2Ref,
+    image3Ref,
+    image4Ref,
+    image5Ref,)
 
   const handleHover = () => {
     gsap.to(explore.current, { width: "100%", duration: 1, ease: 'power2.inOut' });
@@ -35,15 +52,15 @@ const Explore = () => {
   return <section className="flex  xl:h-[30%]  gap-0 p-0">
     {/* TODO */}
     <div className="flex flex-col lg:flex-row xl:flex-1  w-screen  ease-linear duration-500 xl:overflow-x-clip  ">
-      <img className="lg:grow object-cover lg:max-w-[23rem] xl:max-w-[24rem] xl:w-[18rem] flex xl:flex-1" src={data1[currentIndex1]} />
+      <img ref={image1Ref} className="lg:grow object-cover lg:max-w-[23rem] xl:max-w-[24rem] xl:w-[18rem] flex xl:flex-1" src={data1[currentIndex1]} />
       <div className="flex flex-col lg:flex-row  xl:flex-4 xl:whitespace-nowrap">
         <div className="flex flex-row lg:flex-col  ">
-          <img className="flex-grow flex-1 max-h-[12rem] md:max-h-[25rem] object-cover " src={data2[currentIndex2]} />
-          <img className="flex-grow flex-1 max-h-[12rem] md:max-h-[25rem] object-cover " src={data3[currentIndex3]} />
+          <img ref={image2Ref}  className="flex-grow flex-1 max-h-[12rem] md:max-h-[25rem] object-cover " src={data2[currentIndex2]} />
+          <img ref={image3Ref} className="flex-grow flex-1 max-h-[12rem] md:max-h-[25rem] object-cover " src={data3[currentIndex3]} />
         </div>
         <div className="flex flex-row lg:flex-col ">
-          <img className="flex-grow max-h-[12rem] md:max-h-[25rem] flex-1 object-cover " src={data4[currentIndex4]} />
-          <img className="flex-grow max-h-[12rem] md:max-h-[25rem] flex-1 object-cover " src={data5[currentIndex5]} />
+          <img ref={image4Ref} className="flex-grow max-h-[12rem] md:max-h-[25rem] flex-1 object-cover " src={data4[currentIndex4]} />
+          <img ref={image5Ref} className="flex-grow max-h-[12rem] md:max-h-[25rem] flex-1 object-cover " src={data5[currentIndex5]} />
         </div>
       </div>
     </div>
--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -1,58 +1,94 @@
 import { useEffect, useState } from "react";
+import gsap from "gsap";
 
-const useInterval =(data1, data2, data3, data4, data5)=>{
-    const [currentIndex1, setCurrentIndex1] = useState(0);
-    const [currentIndex2, setCurrentIndex2] = useState(0);
-    const [currentIndex3, setCurrentIndex3] = useState(0);
-    const [currentIndex4, setCurrentIndex4] = useState(0);
-    const [currentIndex5, setCurrentIndex5] = useState(0);
+const useInterval = (
+  data1,
+  data2,
+  data3,
+  data4,
+  data5,
+  image1Ref,
+  image2Ref,
+  image3Ref,
+  image4Ref,
+  image5Ref
+) => {
+  const [currentIndex1, setCurrentIndex1] = useState(0);
+  const [currentIndex2, setCurrentIndex2] = useState(0);
+  const [currentIndex3, setCurrentIndex3] = useState(0);
+  const [currentIndex4, setCurrentIndex4] = useState(0);
+  const [currentIndex5, setCurrentIndex5] = useState(0);
 
-    useEffect(() => {
-        const intervals = [];
+  useEffect(() => {
+    // Function to animate dissolving effect using GSAP
+    const animateDissolve = (element) => {
+      gsap.to(element, {
+        opacity: 0,
+
+        onComplete: () => {
+          gsap.to(element, {
+            opacity: 0.5,
     
-        if (data1 && data1.length > 0) {
-            const interval1 = setInterval(() => {
-                setCurrentIndex1((prevIndex) => (prevIndex + 1) % data1.length);
-            }, 2000);
-            intervals.push(interval1);
-        }
-    
-        if (data2 && data2.length > 0) {
-            const interval2 = setInterval(() => {
-                setCurrentIndex2((prevIndex) => (prevIndex + 1) % data2.length);
-            }, 2500);
-            intervals.push(interval2);
-        }
-    
-        if (data3 && data3.length > 0) {
-            const interval3 = setInterval(() => {
-                setCurrentIndex3((prevIndex) => (prevIndex + 1) % data3.length);
-            }, 3000);
-            intervals.push(interval3);
-        }
-    
-        if (data4 && data4.length > 0) {
-            const interval4 = setInterval(() => {
-                setCurrentIndex4((prevIndex) => (prevIndex + 1) % data4.length);
-            }, 3200);
-            intervals.push(interval4);
-        }
-    
-        if (data5 && data5.length > 0) {
-            const interval5 = setInterval(() => {
-                setCurrentIndex5((prevIndex) => (prevIndex + 1) % data5.length);
-            }, 3500);
-            intervals.push(interval5);
-        }
-        
-        return () => {
-            intervals.forEach((interval) => clearInterval(interval));
-        };
-    }, []);
+          });
+        },
+      });
+    };
 
-    return {currentIndex1, currentIndex2, currentIndex3, currentIndex4, currentIndex5}
-}
+    const intervals = [];
 
+    if (data1 && data1.length > 0) {
+      const interval1 = setInterval(() => {
 
-export default useInterval
+            animateDissolve(image1Ref.current);
+        setCurrentIndex1((prevIndex) => (prevIndex + 1) % data1.length);
+      }, 2000);
+      intervals.push(interval1);
+    }
 
+    if (data2 && data2.length > 0) {
+      const interval2 = setInterval(() => {
+        setCurrentIndex2((prevIndex) => (prevIndex + 1) % data2.length);
+        animateDissolve(image2Ref.current);
+      }, 2000);
+      intervals.push(interval2);
+    }
+
+    if (data3 && data3.length > 0) {
+      const interval3 = setInterval(() => {
+        setCurrentIndex3((prevIndex) => (prevIndex + 1) % data3.length);
+        animateDissolve(image3Ref.current);
+      }, 3000);
+      intervals.push(interval3);
+    }
+
+    if (data4 && data4.length > 0) {
+      const interval4 = setInterval(() => {
+        animateDissolve(image4Ref.current);
+        setCurrentIndex4((prevIndex) => (prevIndex + 1) % data4.length);
+      }, 3200);
+      intervals.push(interval4);
+    }
+
+    if (data5 && data5.length > 0) {
+      const interval5 = setInterval(() => {
+        animateDissolve(image5Ref.current);
+        setCurrentIndex5((prevIndex) => (prevIndex + 1) % data5.length);
+      }, 3500);
+      intervals.push(interval5);
+    }
+
+    return () => {
+      intervals.forEach((interval) => clearInterval(interval));
+    };
+  }, []);
+
+  return {
+    currentIndex1,
+    currentIndex2,
+    currentIndex3,
+    currentIndex4,
+    currentIndex5,
+  };
+};
+
+export default useInterval;
