e89c0cdf2d88d402e002a4bbf1ad766789c01e13
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-08-07 12:38:39 +0100

    edits

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -1,5 +1,5 @@
+import { gsap } from "gsap";
 import { useEffect, useState } from "react";
-import gsap from "gsap";
 
 const useInterval = (
   data1,
@@ -20,13 +20,12 @@ const useInterval = (
   const [currentIndex5, setCurrentIndex5] = useState(0);
 
   useEffect(() => {
-
+    const intervals = [];
     const animateFadeOut = (element) => {
       gsap.fromTo(
         element,
         {
           opacity: 0.5,
-       
           // Set the transform origin to the center
           ease: "power1.easeOut",
         },
@@ -39,67 +38,53 @@ const useInterval = (
         }
       );
     };
-      // Function to animate fading in the next image
-    //   const animateFadeIn = (element) => {
-    //     gsap.to(element, {
-    //       opacity: 1,
-    //       duration: 1
-    //     });
-    //   };
-    const intervals = [];
+  
+   
 
     if (data1 && data1.length > 0) {
       const interval1 = setInterval(() => {
-        setCurrentIndex1((prevIndex) => (prevIndex + 1) % data1.length); 
-        animateFadeOut(image1Ref.current);
-      },3000);
+        setCurrentIndex1((prevIndex) => (prevIndex + 1) % data1.length);
+        animateFadeOut(image1Ref);
+      }, 2000);
       intervals.push(interval1);
     }
 
     if (data2 && data2.length > 0) {
       const interval2 = setInterval(() => {
+        animateFadeOut(image2Ref);
         setCurrentIndex2((prevIndex) => (prevIndex + 1) % data2.length);
-          animateFadeOut(image2Ref.current);
-      }, 3000);
+      }, 2500);
       intervals.push(interval2);
     }
 
     if (data3 && data3.length > 0) {
       const interval3 = setInterval(() => {
         setCurrentIndex3((prevIndex) => (prevIndex + 1) % data3.length);
-          animateFadeOut(image3Ref.current);
+        animateFadeOut(image3Ref);
       }, 3000);
       intervals.push(interval3);
     }
 
     if (data4 && data4.length > 0) {
       const interval4 = setInterval(() => {
-          animateFadeOut(image4Ref.current);
         setCurrentIndex4((prevIndex) => (prevIndex + 1) % data4.length);
-      }, 3000);
+        animateFadeOut(image4Ref);
+      }, 3200);
       intervals.push(interval4);
     }
 
     if (data5 && data5.length > 0) {
       const interval5 = setInterval(() => {
-          animateFadeOut(image5Ref.current);
         setCurrentIndex5((prevIndex) => (prevIndex + 1) % data5.length);
-      }, 3000);
+        animateFadeOut(image5Ref);
+      }, 3500);
       intervals.push(interval5);
     }
 
     return () => {
       intervals.forEach((interval) => clearInterval(interval));
     };
-  }, [  image1Ref,
-    image2Ref,
-    image3Ref,
-    image4Ref,
-    image5Ref,  data1,
-    data2,
-    data3,
-    data4,
-    data5,]);
+  }, []);
 
   return {
     currentIndex1,
